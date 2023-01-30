b77bfbdbcce95daa53a4b13bd32fd11f59a96b5f
Radentscheid OS <info@radentscheid-os.de>
2023-01-30 11:24:06 +0100

    create weekly overview of ramp availability diagram

--- verkehrswatch_os/base/utils.py
+++ verkehrswatch_os/base/utils.py
@@ -1,7 +1,16 @@
 from datetime import datetime
 from pytz import timezone
 
-def utc_to_berlin(datetime_string: str):
-    dt = datetime.strptime(datetime_string,"%Y-%m-%d %H:%M:%S")
+def utc2cet(dt: str | datetime):
+    if not isinstance(dt, datetime):
+        dt = datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
     dt = timezone("UTC").localize(dt)
     dt = dt.astimezone(timezone("Europe/Berlin"))
+    return datetime.strftime(dt, "%Y-%m-%d %H:%M:%S")
+
+def cet2utc(dt: str | datetime) :
+    if not isinstance(dt, datetime):
+        dt = datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
+    dt = timezone("Europe/Berlin").localize(dt)
+    dt = dt.astimezone(timezone("UTC"))
+    return datetime.strftime(dt, "%Y-%m-%d %H:%M:%S")
--- verkehrswatch_os/parking/parking_ramps.py
+++ verkehrswatch_os/parking/parking_ramps.py
@@ -4,6 +4,7 @@ import json
 from datetime import datetime
 from base.request import send_request
 from parking.database import query
+from base.utils import utc2cet, cet2utc
 
 def get_ramps_overview(url, onlyOsna = True):
   response = send_request(url)
@@ -31,6 +32,29 @@ def get_ramps_utilization(url):
         exit(2)
     return json.loads(response.content)
 
+def ramp_is_closed(ramp_id, dt_str: str):
+  dt = datetime.strptime(dt_str,"%Y-%m-%d %H:%M:%S")
+  day_of_the_week = dt.isoweekday()
+  time = datetime.strftime(dt, "%H:%M")
+  result = query(
+    """
+      SELECT 
+      CASE 
+        WHEN 
+          ? >= open_time AND ? <= close_time 
+        THEN 
+          False 
+        ELSE True 
+      END
+      from business_hours bh 
+      WHERE ramp_id = ?
+      and day = ?
+    """,
+    (time, time, ramp_id, day_of_the_week)
+  )
+  return result[0] if len(result) > 0 else False
+
+
 def get_ramps_utilization_from_db(city_center: bool, latest: bool = True, time_from: datetime = None, time_to: datetime = datetime.now() ):
   if latest:
     statement = """
@@ -48,5 +72,36 @@ def get_ramps_utilization_from_db(city_center: bool, latest: bool = True, time_f
     return result[0]
   
   else:
-    # TODO: implement time_from and time_to
-    pass
+    # get all ramps
+    rows_ramp_data = query(
+      """
+        SELECT ramp_id, capacity, available, created_at_utc
+        FROM ramps_details rd LEFT JOIN ramp_utilization ru ON rd.id = ru.ramp_id  
+        WHERE
+          rd.city_center = ? AND
+          created_at_utc > ? AND
+          created_at_utc < ?
+      """,
+      (city_center, cet2utc(time_from), cet2utc(time_to))
+    )
+
+    # Loop through ramps
+    availablity_dict = dict()
+    for row in rows_ramp_data:
+      time_cet = utc2cet(row["created_at_utc"])
+      # check if ramp is open
+      
+      is_closed = ramp_is_closed(row["ramp_id"], time_cet)
+      capacity = row["capacity"]
+      # if ramp is closed, assume it is empty and all parking spots are available (data shows none available, which is actually wrong, since there is a lot of unused space)
+      available = row["available"] if not is_closed else capacity
+      # add values to dict
+      if time_cet in availablity_dict.keys():
+          availablity_dict[time_cet]["capacity"] = availablity_dict[time_cet]["capacity"] + capacity
+          availablity_dict[time_cet]["available"] = availablity_dict[time_cet]["available"] + available
+      else:
+        availablity_dict[time_cet] = {
+          "capacity": row["capacity"],
+          "available": row["available"]
+        }
+    return availablity_dict
